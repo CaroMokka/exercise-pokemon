eec5f48e74e5dcf6b54523b18de18e041038ce15
carolina <ci.arayagonzalez@gmail.com>
2023-06-25 19:07:54 -0400

    style: add style cards

--- getPokemones.js
+++ getPokemones.js
@@ -1,33 +1,43 @@
 // import axios from "axios"
 document.addEventListener("DOMContentLoaded", function () {
-  //Consulta a API
-const getPokemones = (async () => {
+  //API
+  const getPokemones = (async () => {
     try {
-      // const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=30";
-    const dataRaw = await axios(URL);
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=20";
+      const dataRaw = await axios(URL);
       // const data = await dataRaw.json()
-    console.log("data: ", dataRaw.data.results);
-    return dataRaw.data.results;
+      console.log("data: ", dataRaw.data.results);
+      const pokemones = dataRaw.data.results;
+      const arrayPoke = [];
+      for (let i = 0; i < pokemones.length; i++) {
+        let pokeUrl = pokemones[i].url;
+        arrayPoke.push(pokeUrl);
+      }
+      return arrayPoke;
     } catch (error) {
-    console.log("error es: ", error);
+      console.log("error es: ", error);
     }
-})();
+  })();
+  console.log(getPokemones);
+  //create pokemon Card
+  const createPokemon = getPokemones.then((res) => {
+    const urls = res;
+    console.log(urls)
+    urls.forEach( async (url) => {
+      const poke = await axios(url);
+      const nombre = poke.data.name
+      const image = poke.data.sprites.other.dream_world.front_default
+      const id = poke.data.id
+      //cards
+      const container = document.getElementById('container-cards')
+      const card = document.createElement('div')
+      card.className = 'card-style'
+      container.appendChild(card)
+      card.innerHTML = `<h2 style="color:red">${nombre}</h2><img src=${image} style="width:150px"></img><p style="text-align:center">#${id}</p>`
+     
+      
 
-  // Ejemplo de utilizacion de la data que responde la API, en otro script
-const pokemones = getPokemones.then((res) => {
-    const allPokemones = res;
-
-    for (let i = 0; i < allPokemones.length; i++) {
-        let element = allPokemones[i];
-        let p = document.createElement("p");
-        //probar con setAtribute para css
-        p.textContent = element.name;
-        //   console.log(element.name);
-        const container = document.getElementById("container-pokemon");
-        container.appendChild(p);
-    }
-    })
-    .catch((error) => console.log("error", error));
-    console.log(pokemones)
+    });
+  }).catch((error) => console.log("error", error));
+  console.log(createPokemon)
 });
