6aa81ecd0656648d3449dbde540f062b695c38ed
carolina <ci.arayagonzalez@gmail.com>
2023-06-25 01:06:41 -0400

    ft - basic render of elements

--- getPokemones.js
+++ getPokemones.js
@@ -1,17 +1,33 @@
 // import axios from "axios"
-
-export const getPokemones = (async () => {
-try {
-    // const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=30"
-    const dataRaw = await axios(URL)
-    // const data = await dataRaw.json()
-    console.log("data: ", dataRaw.data.results[0].name)
-} catch (error) {
-    console.log("error es: ", error)
-}
+document.addEventListener("DOMContentLoaded", function () {
+  //Consulta a API
+const getPokemones = (async () => {
+    try {
+      // const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=30";
+    const dataRaw = await axios(URL);
+      // const data = await dataRaw.json()
+    console.log("data: ", dataRaw.data.results);
+    return dataRaw.data.results;
+    } catch (error) {
+    console.log("error es: ", error);
+    }
 })();
-//IIFE --> Immediately Invoked Function Expressions
 
-const nombrePokemon = document.getElementById("name-pokemon");
-console.log("pokemon: ", nombrePokemon)
+  // Ejemplo de utilizacion de la data que responde la API, en otro script
+const pokemones = getPokemones.then((res) => {
+    const allPokemones = res;
+
+    for (let i = 0; i < allPokemones.length; i++) {
+        let element = allPokemones[i];
+        let p = document.createElement("p");
+        //probar con setAtribute para css
+        p.textContent = element.name;
+        //   console.log(element.name);
+        const container = document.getElementById("container-pokemon");
+        container.appendChild(p);
+    }
+    })
+    .catch((error) => console.log("error", error));
+    console.log(pokemones)
+});
